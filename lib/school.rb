ed0eae6c7af7efa3fd0abcdb8b9474ff9774dbb6
Daniel Gould <dmgould310@gmail.com>
2020-08-07 21:03:57 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,31 +1,15 @@
 class School
-  attr_accessor :name, :roster
-
-  
-  def initialize(name)
-  @name = name 
-  @roster = {} 
-  end 
-  
-  def add_student(name, grade)
-    if @roster.has_key?(grade)
-      @roster[grade].push(name)
-    else 
-      @roster[grade] = [name]
-    end 
-  end 
-  
-  def grade(grade)
-    @roster[grade]
-  end 
-  
-  def sort 
-    self.roster.each do |grade, name|
-      @roster[grade] = name.sort 
-      end 
-  end 
+ attr_accessor :name 
+ 
+ def initialize(name)
+   @name = name 
+ end 
   
   
-
-
+  
+  
 end 
+  
+  
+
+
